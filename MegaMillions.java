1757bb2c163b8d16eb1b4935fc25efb4163a7bb7
Jay-Wolff <54644183+Jay-Wolff@users.noreply.github.com>
2019-09-03 16:51:25 -0400

    Add files via upload

---
+++ MegaMillions.java
@@ -0,0 +1,32 @@
+	import java.util.Random;
+
+//1. name conventions should start with a letter, $, or underscore.
+	//2. classes should start with a capital letter 'no spaces'
+	//3. functions and variables should start with lower cases
+	//4. do not use restricted key words as the name of either classes,functions, projects, packages, or variables
+	//write a program that simulates the Mega millions
+	//it will ask the user for the amount of dollars to play
+	//it will ask the user to enter 6 numbers
+	// randomly generate the numbers -> compare if there is a match
+	//scanner for input is used
+public class MegaMillion {
+	public static void main(String[] args) {
+		//allows you to generate random values 
+		Random randGen= new Random();
+		int rnum1 = randGen.nextint(60)+1;
+		System.out.println(rnum1);
+		//Declare variable to hold amount of dollars
+		//Declare variable for cost of ticket
+	
+		//Prompt the user for the amount of dollars to bet on
+		
+		//store amount into amount of dollar variable
+		
+		//Prompt the user to enter six numbers 
+		//	create a variable for each value 
+		
+		//Randomly generate six numbers 
+
+	
+ }
+}
