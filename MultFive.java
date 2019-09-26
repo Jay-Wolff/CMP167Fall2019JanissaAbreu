7043edb124c17f5316cc6ca0c97290a94f45cc19
Jay-Wolff <54644183+Jay-Wolff@users.noreply.github.com>
2019-09-25 17:57:34 -0700

    Add files via upload

---
+++ MultFive.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author abreu
+ *
+ */
+
+import java.util.Scanner;
+public class MultFive {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+			System.out.println("Welcome");
+			System.out.println("Enter five int values");
+			int num1 = input.nextInt();
+			int num2 = input.nextInt();
+			int num3 = input.nextInt();
+			int num4 = input.nextInt();
+			int num5 = input.nextInt();
+			int Result = (num1 + num2 + num3 + num4 + num5);
+			System.out.println(Result);
+					
+	}
+
+}
