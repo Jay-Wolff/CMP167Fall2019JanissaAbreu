794f4455e45f6e5d315c6f46ac0ea1c0e27ed001
Jay-Wolff <54644183+Jay-Wolff@users.noreply.github.com>
2019-11-26 16:11:17 -0500

    Add files via upload

---
+++ Employee1.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Student
+ *
+ */
+public class Employee1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
